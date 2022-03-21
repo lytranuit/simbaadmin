948285d09a7bc702ec56695e317757d1c1ec6167
lytranuit <lytranuit@gmail.com>
2022-03-21 16:42:20 +0700

    update

--- app/Views/backend/sale/index.php
+++ app/Views/backend/sale/index.php
@@ -47,6 +47,7 @@
 
 <script type="text/javascript">
     $(document).ready(function() {
+        let list_status = <?= json_encode($status) ?>;
         $('#quanlytin').DataTable({
             "stateSave": true,
             "processing": true,
@@ -55,6 +56,20 @@
                 "url": path + "admin/sale/table",
                 "dataType": "json",
                 "type": "POST",
+                'data': function(data) {
+                    // Read values
+                    let orders = data['order'];
+                    for (let i in orders) {
+                        let order = orders[i];
+                        let column = order['column'];
+                        orders[i]['data'] = data['columns'][column]['data'];
+                    }
+                    let search_type = localStorage.getItem('SEARCH_TYPE') || "code";
+                    let search_status = localStorage.getItem('SEARCH_STATUS') || "0";
+                    data['search_type'] = search_type;
+                    data['search_status'] = search_status;
+                    data['<?= csrf_token() ?>'] = "<?= csrf_hash() ?>";
+                }
             },
             "columns": [{
                     "data": "id"
@@ -80,7 +95,41 @@
                 {
                     "data": "action"
                 }
-            ]
+            ],
+            initComplete: function() {
+                $(".dataTables_filter label").prepend("<select style='margin-right: 0.5em;display: inline-block;width: auto;' class='form-control form-control-sm search_type'><option value='id'>ID</option><option value='code'>Mã đơn hàng</option><option value='status'>Trạng thái</option><option value='description_vi'>Ghi chú</option></select>");
+                $(".dataTables_filter label").append("<select style='margin-left: 0.5em;display: inline-block;width: auto;' class='form-control form-control-sm search_status d-none'></select>");
+                let html = "";
+
+                html += "<option value='-1'>-- Select --</option>";
+                html += "<option value='1'>Mới đặt hàng</option>";
+                html += "<option value='2'>Đã xác nhận, chờ giao</option>";
+                html += "<option value='8'>Đang giao hàng</option>";
+                html += "<option value='3'>Đã thanh toán</option>";
+                html += "<option value='4'>Hoàn tất giao hàng</option>";
+                html += "<option value='5'>Đã hủy</option>";
+
+                $(".search_status").append(html);
+
+
+
+                let search_type = localStorage.getItem('SEARCH_TYPE') || "code";
+                $(".search_type").val(search_type);
+
+                if (search_type == "status") {
+                    $(".search_status").removeClass("d-none");
+                    $(".dataTables_filter label input").addClass("d-none");
+                } else {
+                    $(".search_status").addClass("d-none");
+                    $(".dataTables_filter label input").removeClass("d-none");
+                }
+
+
+
+                let search_status = localStorage.getItem('SEARCH_STATUS') || "0";
+                $(".search_status").val(search_status);
+
+            }
 
         });
     });
